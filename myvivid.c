186ecfe27ea64e87c57f87ea21ed8222ee663164
100ask <user@100ask.com>
2023-05-22 09:49:41 -0400

    add ioctl "vidioc_querycap"

--- myvivid.c
+++ myvivid.c
@@ -23,10 +23,22 @@
 static struct video_device *my_vivid_device;
 struct v4l2_device v4l2_dev;
 
-
+//应用程序通过ioctl()查询性能？看它是不是摄像头设备
+static int my_vivid_vidioc_querycap(struct file *file, void  *priv,
+					                        struct v4l2_capability *cap)
+{
+    printk("enter %s\n", __func__);
+	strcpy(cap->driver, "myvivid");
+	strcpy(cap->card, "myvivid");
+    cap->version = 0x0001;
+	//capabilities表明是视频的捕捉设备、V4L2_CAP_STREAMING表明可以通过ioctl读取数据
+	cap->capabilities =	V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_STREAMING;
+	return 0;
+}
 
 static const struct v4l2_file_operations my_vivid_fops = {
 	.owner = THIS_MODULE,
+    //.unlocked_ioctl = video_ioctl2,/* V4L2 ioctl handler */
 };
 
 
@@ -37,21 +49,23 @@ static void my_vivid_release(struct video_device *vdev)
 
 static const struct v4l2_ioctl_ops my_vivid_ioctl_ops =
 {
-    // // 表示它是一个摄像头设备
-    // .vidioc_querycap          = my_vivid_vidioc_querycap,
-    // /* 用于列举、获得、测试、设置摄像头的数据的格式 */
-    // .vidioc_enum_fmt_vid_cap  = my_vivid_vidioc_enum_fmt_vid_cap,
-    // .vidioc_g_fmt_vid_cap     = my_vivid_vidioc_g_fmt_vid_cap,
-    // .vidioc_try_fmt_vid_cap   = my_vivid_vidioc_try_fmt_vid_cap,
-    // .vidioc_s_fmt_vid_cap     = my_vivid_vidioc_s_fmt_vid_cap,
-    // /* 缓冲区操作: 申请/查询/放入队列/取出队列 */
-    // .vidioc_reqbufs       = my_vivid_vidioc_reqbufs,
-    // .vidioc_querybuf      = my_vivid_vidioc_querybuf,
-    // .vidioc_qbuf          = my_vivid_vidioc_qbuf,
-    // .vidioc_dqbuf         = my_vivid_vidioc_dqbuf,
-    // // 启动/停止
-    // .vidioc_streamon      = my_vivid_vidioc_streamon,
-    // .vidioc_streamoff     = my_vivid_vidioc_streamoff,
+    // 表示它是一个摄像头设备
+    .vidioc_querycap          = my_vivid_vidioc_querycap,
+#if 0
+    /* 用于列举、获得、测试、设置摄像头的数据的格式 */
+    .vidioc_enum_fmt_vid_cap  = my_vivid_vidioc_enum_fmt_vid_cap,
+    .vidioc_g_fmt_vid_cap     = my_vivid_vidioc_g_fmt_vid_cap,
+    .vidioc_try_fmt_vid_cap   = my_vivid_vidioc_try_fmt_vid_cap,
+    .vidioc_s_fmt_vid_cap     = my_vivid_vidioc_s_fmt_vid_cap,
+    /* 缓冲区操作: 申请/查询/放入队列/取出队列 */
+    .vidioc_reqbufs       = my_vivid_vidioc_reqbufs,
+    .vidioc_querybuf      = my_vivid_vidioc_querybuf,
+    .vidioc_qbuf          = my_vivid_vidioc_qbuf,
+    .vidioc_dqbuf         = my_vivid_vidioc_dqbuf,
+    // 启动/停止
+    .vidioc_streamon      = my_vivid_vidioc_streamon,
+    .vidioc_streamoff     = my_vivid_vidioc_streamoff,
+#endif
 };
 
 static int __init my_vivid_init(void)
@@ -64,8 +78,8 @@ static int __init my_vivid_init(void)
     my_vivid_device->release = my_vivid_release;
     my_vivid_device->v4l2_dev  = &v4l2_dev;
     my_vivid_device->fops = &my_vivid_fops;
-    //myvivid_device->ioctl_ops = &my_vivid_ioctl_ops;
-    /*3  注册*/
+    my_vivid_device->ioctl_ops = &my_vivid_ioctl_ops;
+    /*3  注册*/ 
     //type 参考vivid-core.c  -nr 参考函数说明 -1 表示第一个可用设备
     error = video_register_device(my_vivid_device,VFL_TYPE_GRABBER, -1);
 
